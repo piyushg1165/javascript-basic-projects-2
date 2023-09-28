16b23476ba46a0022b970e5f70a8c96d0340a1a2
piyushg1165 <piyushgupta1165@gmail.com>
2023-09-28 12:04:20 +0530

    1 file modified

---
+++ 4-GuessTheNumber/chaiaurcode.js
@@ -0,0 +1,38 @@
+const ranum = parseInt(Math.random() * 100 + 1);
+
+const submit = document.querySelector('#subt');
+const userInput = document.querySelector('#guessField');
+const guessSlot = document.querySelector('.guesses');
+const remaining = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+const startOver = document.querySelector('.resultParas');
+
+const p = document.createElement('p');
+
+let playGame = true;
+let preGuess = [];
+let numGuess = 1;
+
+function validateGuess(guess){
+  //
+}
+
+function checkGuess(guess){
+  //
+}
+
+function displayGuess(guess){
+  //
+}
+
+function displayMessage(medssage){
+  //
+}
+function newGame(){
+  //
+}
+function endGame(){
+  //
+}
+
+
