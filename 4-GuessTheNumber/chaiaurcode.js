5a713363ba3a4eb82e3170c18c09663518da9f70
piyushg1165 <piyushgupta1165@gmail.com>
2023-09-28 14:05:43 +0530

    2 files modified

--- 4-GuessTheNumber/chaiaurcode.js
+++ 4-GuessTheNumber/chaiaurcode.js
@@ -1,4 +1,4 @@
-const ranum = parseInt(Math.random() * 100 + 1);
+let randomNumber = parseInt(Math.random() * 100 + 1);
 
 const submit = document.querySelector('#subt');
 const userInput = document.querySelector('#guessField');
@@ -10,23 +10,61 @@ const startOver = document.querySelector('.resultParas');
 const p = document.createElement('p');
 
 let playGame = true;
-let preGuess = [];
+let prevGuess = [];
 let numGuess = 1;
+
+if(playGame){
+  submit.addEventListener('click', function(e){
+    e.preventDefault();
+    const guess = parseInt(userInput.value);
+    validateGuess(guess);
+  })
+}
 
 function validateGuess(guess){
   //
+  if((guess < 0 && guess >100) || isNaN(guess)){
+    alert('Please enter a valid number')
+  }
+  else{
+    prevGuess.push(guess);
+    if(numGuess > 10){
+      displayGuess(guess);
+      displayMessage(`Game over ! Random Number was ${randomNumber}`);
+      endGame();
+    }
+    else{
+      displayGuess(guess);
+      checkGuess(guess);
+    }
+  }
 }
 
 function checkGuess(guess){
   //
+  if(guess === randomNumber){
+    displayMessage('YOU WON!!');
+    endGame();
+  }
+  else if(guess < randomNumber){
+    displayMessage('Your guess is TOOO LOW!');
+  }
+  else if(guess > randomNumber){
+    displayMessage('Your guess is TOOO HIGH!');
+  }
 }
 
 function displayGuess(guess){
   //
+  userInput.value = '';
+  guessSlot.innerHTML += `${guess} , `;
+  numGuess++;
+  remaining.innerHTML = `${11 - numGuess} ` ;
 }
 
-function displayMessage(medssage){
+function displayMessage(message){
   //
+  lowOrHi.innerHTML = `<h2>${message}</h2>`;
 }
 function newGame(){
   //
